3c22f6c28d7c6432d927bc5895c0d05f2cb0acb1
Carlos del Prado <carlos.delprado@11paths.com>
2020-11-06 21:03:50 +0000

    feat: counter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
-const counter = new Counter();
+
+let counter = new Counter();
 counter.onCountListener = count => console.log(`The count is:`, count);
 counter.start();
 
 setTimeout(() => counter.stop(), 10000);
 
+
+function printWithTimeouts() {
+  console.log('a'); // first
+  setTimeout(() => console.log('b'), 1000); // fourth
+  setTimeout(() => console.log('c'), 500); // third
+  console.log('d'); // second
+}
+
+printWithTimeouts();
